5c44af48283f27566e0e5b22f7b87092e8b0b6c2
JesusLazoG <jesuslg@e-quipu.pe>
2016-08-05 12:15:20 -0500

    actualizacion 3

--- index.php
+++ index.php
@@ -4,3 +4,45 @@ include('externo.php');
 $data = externo::datos();
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PRUEBA</title>
+	<script src="jquery-3.1.0.min.js"> </script>
+	<script src="/bootstrap/bootstrap.min.js"> </script>
+	<script src="/prueba.js"> </script>
+	<link rel="stylesheet" href="/prueba.css">
+	<link rel="stylesheet" href="/bootstrap/bootstrap.min.css">
+
+</head>
+<body>
+<div>
+	<table class="table table-hover table-responsive">
+  				<thead>
+  					<tr>
+  						<th>First Name</th>
+  						<th>Last Name</th>
+  						<th>“Email</th>
+  					</tr>
+  				</thead>
+  				<tbody>
+  					<tr>
+  						<td></td>
+  						<td></td>
+  						<td></td>
+  					</tr>
+  				</tbody>
+		</table>
+
+
+</div>
+
+
+
+
+
+</body>
+</html>
+
